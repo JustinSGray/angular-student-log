704cf40ad450d7e92534a6a9f7d510c581bdc81b
Justin Gray <Justin.S.Gray@gmail.com>
2012-10-30 22:22:23 -0400

    started student rows

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -27,6 +27,22 @@ klasses.$inject = ['$scope','Klass'];
 
 function klass($scope,Klass,$routeParams) {
     var klass = $scope.klass = Klass.get({'classId':$routeParams.classId})
+//Msg#SepIDLast NameFirst NameDECP1 P2TeacherStatusParent's NameGrPhone #R inW inR outW out
+    $scope.header_map = [{"key":"sep_id","value":"SepID"},
+                         {"key":"last_name","value":"Last Name"},
+                         {"key":"first_name","value":'First Name'},
+                         {"key":"DEC","value":'DEC'},
+                         {"key":"p1","value":"P1"},
+                         {"key":"p2","value":"P2"},
+                         {"key":"Teacher","value":"Teacher"},
+                         {"key":"status","value":"Status"},
+                         {"key":"parents_name","value":"Parent's Name"},
+                         {"key":"grade","value":"Gr"},
+                         {"key":"phone","value":"Phone #"},
+                         {"key":"r_in","value":"R in"},
+                         {"key":"w_in","value":"W in"},
+                         {"key":"r_out","value":"R out"},
+                         {"key":"w_out","value":"W out"}];
 }
 klass.$inject = ['$scope','Klass','$routeParams'];
 
